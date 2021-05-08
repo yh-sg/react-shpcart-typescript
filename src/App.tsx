ce506ed60527aa4d525b8127b356c46af60bf993
yonghan <wangyh.92@gmail.com>
2021-05-08 15:58:40 +0800

    without styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import {useQuery} from 'react-query';
 //Components
+import Item from './item/item'
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -27,18 +28,33 @@ const getProducts = async ():Promise<CartItemType[]> => {
 
 const App:React.FC = () => {
 
+  //https://react-query.tanstack.com/overview
   const {data, isLoading, error} = useQuery<CartItemType[]>
   (
     'products', 
     getProducts
   );
-  console.log(data);
   
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>Something went wrong...</div>
 
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {/* ? is for if nothing, return undefined instead of throwing error */}
+        {data?.map(item=>(
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
