c704bb7312d5aa76f9ba52484d80e49e1fc4c413
yonghan <wangyh.92@gmail.com>
2021-05-08 15:13:29 +0800

    tested with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {useQuery} from 'react-query';
+//Components
+import Drawer from '@material-ui/core/Drawer';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+import Badge from '@material-ui/core/Badge'
+//Styles
+import {Wrapper} from './App.styles'
 
-function App() {
+export type CartItemType = {
+  category: string;
+  description: string;
+  id: number;
+  image: string;
+  price: number;
+  title: string;
+  amount: number; //my own type, to keep track the total amount
+}
+
+const getProducts = async ():Promise<CartItemType[]> => {
+  const result = await (await fetch('https://fakestoreapi.com/products')).json();
+  console.log(result)
+  return result;
+}
+
+const App:React.FC = () => {
+
+  const {data, isLoading, error} = useQuery<CartItemType[]>
+  (
+    'products', 
+    getProducts
+  );
+  console.log(data);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start
     </div>
   );
 }
